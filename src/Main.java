901eaec93f3c1b363cc14e7f1e227e6e7675b84d
Souvik Sarkar <souviksarkar2k3@gmail.com>
2023-01-19 13:25:20 +0530

    Mainf file created.

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import souvik.graphs.adjMatrix.Graph;
+import souvik.graphs.adjMatrix.DepthFirstSearch;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Graph graph = new Graph(7);
+        graph.addEdge(0,1);
+        graph.addEdge(0,4);
+        graph.addEdge(0,5);
+        graph.addEdge(2,1);
+        graph.addEdge(2,3);
+        graph.addEdge(5,6);
+        DepthFirstSearch dfs = new DepthFirstSearch(graph,1);
+        for(int i : dfs.traversal()){
+            System.out.print(i + " -> ");
+        }
+        System.out.print("Fin");
+    }
+}
