6a3c3768b0206e5e6f34926b1bc4444731e5cd37
Souvik Sarkar <souviksarkar2k3@gmail.com>
2023-01-19 13:26:12 +0530

    List Implementation

---
+++ src/souvik/support/List.java
@@ -0,0 +1,111 @@
+package souvik.support;
+
+import java.util.Iterator;
+
+public class List<Key> implements Iterable<Key> {
+    private Node head;
+    private int size;
+
+    private class Node {
+        public Key key;
+        public Node next;
+
+        public Node(Key key) {
+            this.key = key;
+            next = null;
+        }
+    }
+
+    public List() {
+        head = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public int length() {
+        return size;
+    }
+
+    public void push_front(Key key) {
+        if (isEmpty()) {
+            head = new Node(key);
+        } else {
+            Node temp = new Node(key);
+            temp.next = head;
+            head = temp;
+        }
+        size++;
+    }
+
+    public void push_back(Key key) {
+        if (isEmpty()) {
+            head = new Node(key);
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = new Node(key);
+        }
+        size++;
+    }
+
+    public Key pop_front() {
+        if (isEmpty()) {
+            throw new RuntimeException();
+        }
+        Key key = head.key;
+        head = head.next;
+        size--;
+        return key;
+    }
+
+    public Key pop_back() {
+        if (isEmpty()) {
+            throw new RuntimeException();
+        }
+        if (this.size == 1) {
+            Key key = head.key;
+            head = null;
+            size--;
+            return key;
+        }
+        Node temp = head;
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+        Key key = temp.next.key;
+        temp.next = null;
+        size--;
+        return key;
+    }
+
+    private class ListIterator implements Iterator<Key> {
+        private Node current;
+
+        public ListIterator() {
+            current = head;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public Key next() {
+            Key key = current.key;
+            current = current.next;
+            return key;
+        }
+    }
+
+    @Override
+    public Iterator<Key> iterator() {
+        return new ListIterator();
+    }
+
+}
