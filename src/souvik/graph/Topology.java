78211343fdea4f17cc4c296309e15385b20cc4c2
Souvik Sarkar <souviksarkar2k3@gmail.com>
2023-02-10 01:21:36 +0530

    Topological Sort Implementation for Weighted Directed Graph.

--- src/souvik/graph/Topology.java
+++ src/souvik/graph/Topology.java
@@ -6,35 +6,72 @@ public class Topology {
     private final Stack<Integer> reversePostOrder;
     private final boolean[] marked;
 
-    public Topology(GraphAL graph) {
-        assert !hasCycle(graph);
+    public Topology(Digraph digraph) {
+        assert !hasCycle(digraph);
         reversePostOrder = new Stack<>();
-        marked = new boolean[graph.getVertex()];
-        for (int v = 0; v < graph.getVertex(); v++) {
+        marked = new boolean[digraph.getVertex()];
+        for (int v = 0; v < digraph.getVertex(); v++) {
             if (!marked[v]) {
-                dfs(graph, v);
+                dfs(digraph, v);
             }
         }
     }
 
-    private void dfs(GraphAL graph, int v) {
+    public Topology(WeightedDigraph weightedDigraph) {
+        reversePostOrder = new Stack<>();
+        marked = new boolean[weightedDigraph.getVertex()];
+        for (int v = 0; v < weightedDigraph.getVertex(); v++) {
+            if (!marked[v]) {
+                dfs(weightedDigraph, v);
+            }
+        }
+    }
+
+    private void dfs(Digraph digraph, int v) {
         marked[v] = true;
-        for (int w : graph.adj(v)) {
+        for (int w : digraph.adj(v)) {
             if (!marked[w]) {
-                dfs(graph, w);
+                dfs(digraph, w);
                 reversePostOrder.push(w);
             }
         }
     }
 
-    public boolean hasCycle(GraphAL graph) {
-        boolean[] checked = new boolean[graph.getVertex()];
+    private void dfs(WeightedDigraph weightedDigraph, int v) {
+        marked[v] = true;
+        for (DirectedEdge e : weightedDigraph.adj(v)) {
+            int w = e.to();
+            if (!marked[w]) {
+                dfs(weightedDigraph, w);
+                reversePostOrder.push(w);
+            }
+        }
+    }
+
+    public boolean hasCycle(Digraph digraph) {
+        boolean[] checked = new boolean[digraph.getVertex()];
         Stack<Integer> s = new Stack<>();
         s.push(0);
         while (!s.isEmpty()) {
             int v = s.pop();
             checked[v] = true;
-            for (int w : graph.adj(v)) {
+            for (int w : digraph.adj(v)) {
+                if (checked[w]) return true;
+                s.push(w);
+            }
+        }
+        return false;
+    }
+
+    public boolean hasCycle(WeightedDigraph weightedDigraph) {
+        boolean[] checked = new boolean[weightedDigraph.getVertex()];
+        Stack<Integer> s = new Stack<>();
+        s.push(0);
+        while (!s.isEmpty()) {
+            int v = s.pop();
+            checked[v] = true;
+            for (DirectedEdge e : weightedDigraph.adj(v)) {
+                int w = e.to();
                 if (checked[w]) return true;
                 s.push(w);
             }
