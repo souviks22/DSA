f0c06bd3a111d8a81a1544fdd6379e4d397f19d6
Souvik Sarkar <souviksarkar2k3@gmail.com>
2023-02-10 01:14:27 +0530

    Weighted Directed Graph Implementaion.

--- src/souvik/graph/WeightedDigraph.java
+++ src/souvik/graph/WeightedDigraph.java
@@ -1,2 +1,34 @@
-package souvik.graph;public class WeightedDigraph {
+package souvik.graph;
+
+import souvik.support.List;
+
+public class WeightedDigraph {
+    private final int vertex;
+    private int edges;
+    private final List<DirectedEdge>[] adj;
+
+    public WeightedDigraph(int vertex) {
+        this.vertex = vertex;
+        adj = (List<DirectedEdge>[]) new List[vertex];
+        for (int i = 0; i < vertex; i++) {
+            adj[i] = new List<>();
+        }
+    }
+
+    public void addEdge(DirectedEdge e) {
+        adj[e.from()].pushFront(e);
+        edges++;
+    }
+
+    public List<DirectedEdge> adj(int v) {
+        return adj[v];
+    }
+
+    public int getVertex() {
+        return vertex;
+    }
+
+    public int getEdges() {
+        return edges;
+    }
 }
