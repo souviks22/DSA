680b6a4648a24438451bbc72757bb27b8141e13a
Souvik Sarkar <souviksarkar2k3@gmail.com>
2023-02-12 20:21:23 +0530

    Dijkstra Shortest Path Implementation.

--- src/souvik/shortestPath/DijkstraSP.java
+++ src/souvik/shortestPath/DijkstraSP.java
@@ -3,17 +3,23 @@ package souvik.shortestPath;
 import souvik.graph.DirectedEdge;
 import souvik.graph.WeightedDigraph;
 import souvik.support.IndexMinPQ;
+import souvik.support.Stack;
 
 public class DijkstraSP {
-    private final int source;
     private final IndexMinPQ<Double> pq;
+    private final DirectedEdge[] edgeTo;
 
     public DijkstraSP(WeightedDigraph weightedDigraph, int source) {
-        this.source = source;
-        pq = new IndexMinPQ<>(weightedDigraph.getVertex());
-        for (int i = 0; i < weightedDigraph.getVertex(); i++) pq.enqueue(i,Double.POSITIVE_INFINITY);
-        while(!pq.isEmpty()){
+        int V = weightedDigraph.getVertex();
+        pq = new IndexMinPQ<>(V);
+        edgeTo = new DirectedEdge[V];
+        for (int i = 0; i < V; i++) {
+            if (i == source) pq.enqueue(i, 0.0);
+            else pq.enqueue(i, Double.POSITIVE_INFINITY);
+        }
+        while (!pq.isEmpty()) {
             int v = pq.dequeue();
+            for (DirectedEdge e : weightedDigraph.adj(v)) relax(e);
         }
     }
 
@@ -21,6 +27,20 @@ public class DijkstraSP {
         int v = e.from(), w = e.to();
         if (pq.keys(v) + e.getWeight() < pq.keys(w)) {
             pq.decreaseKey(w, pq.keys(v) + e.getWeight());
+            edgeTo[w] = e;
         }
+    }
+
+    public double getDistTo(int v) {
+        return pq.keys(v);
+    }
+
+    public Stack<DirectedEdge> shortestPathTo(int v) {
+        if (getDistTo(v) == Double.POSITIVE_INFINITY) return null;
+        Stack<DirectedEdge> path = new Stack<>();
+        for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()]) {
+            path.push(e);
+        }
+        return path;
     }
 }
