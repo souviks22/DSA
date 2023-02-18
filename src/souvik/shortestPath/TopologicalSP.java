aff65ce983162f939de519ee1525b7e0c5893c9a
Souvik Sarkar <souviksarkar2k3@gmail.com>
2023-02-18 13:27:26 +0530

    Cycle Detection added in Topological Shortest Path Implementation.

--- src/souvik/shortestPath/TopologicalSP.java
+++ src/souvik/shortestPath/TopologicalSP.java
@@ -10,6 +10,7 @@ public class TopologicalSP {
     private final DirectedEdge[] edgeTo;
 
     public TopologicalSP(WeightedDigraph weightedDigraph, int source) {
+        assert !hasCycle(weightedDigraph);
         int V = weightedDigraph.getVertex();
         distTo = new double[V];
         for (int i = 0; i < V; i++) distTo[i] = Double.POSITIVE_INFINITY;
@@ -19,6 +20,25 @@ public class TopologicalSP {
         for (int v : tp.order()) {
             for (DirectedEdge e : weightedDigraph.adj(v)) relax(e);
         }
+    }
+
+    private boolean hasCycle(WeightedDigraph weightedDigraph) {
+        boolean[] checked = new boolean[weightedDigraph.getVertex()];
+        Stack<Integer> s = new Stack<>();
+        for (int v = 0; v < weightedDigraph.getVertex(); v++) {
+            if (checked[v]) continue;
+            checked[v] = true;
+            boolean[] marked = new boolean[weightedDigraph.getVertex()];
+            s.push(v);
+            while (!s.isEmpty()) {
+                int w = s.pop();
+                if (!checked[w]) checked[w] = true;
+                if (marked[w]) return true;
+                marked[w] = true;
+                for (DirectedEdge e : weightedDigraph.adj(v)) s.push(e.to());
+            }
+        }
+        return false;
     }
 
     private void relax(DirectedEdge e) {
