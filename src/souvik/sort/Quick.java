bada9c32a311a7e222da881effa2dc91c8cf48e5
Souvik Sarkar <souviksarkar2k3@gmail.com>
2023-02-19 02:00:40 +0530

    Quick Select and Three Way Quick Sort Implementation.

--- src/souvik/sort/Quick.java
+++ src/souvik/sort/Quick.java
@@ -1,5 +1,7 @@
 package souvik.sort;
 
+import java.util.Random;
+
 public class Quick {
     private Quick() {
     }
@@ -8,21 +10,36 @@ public class Quick {
         return arr[i].compareTo(arr[j]) < 0;
     }
 
+    private static <Key extends Comparable<Key>> boolean greater(Key[] arr, int i, int j) {
+        return arr[i].compareTo(arr[j]) > 0;
+    }
+
     private static <Key extends Comparable<Key>> void swap(Key[] arr, int i, int j) {
         Key temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
 
+    private static <Key extends Comparable<Key>> void shuffle(Key[] arr) {
+        Random random = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            swap(arr, i, random.nextInt(i + 1));
+        }
+    }
+
     private static <Key extends Comparable<Key>> int partition(Key[] arr, int lo, int hi) {
         int i = lo + 1, j = hi;
         while (true) {
-            while (!less(arr, 0, i)) i++;
-            while (!less(arr, j, 0)) j--;
+            while (!less(arr, lo, i)) {
+                if (++i == hi) break;
+            }
+            while (!less(arr, j, lo)) {
+                if (--j == lo) break;
+            }
             if (i >= j) break;
             swap(arr, i, j);
         }
-        swap(arr, 0, j);
+        swap(arr, lo, j);
         return j;
     }
 
@@ -34,6 +51,48 @@ public class Quick {
     }
 
     public static <Key extends Comparable<Key>> void sort(Key[] arr) {
+        shuffle(arr);
         sort(arr, 0, arr.length - 1);
+    }
+
+    public static <Key extends Comparable<Key>> Key select(Key[] arr, int k) {
+        int i = 0, j = arr.length - 1;
+        while (true) {
+            int p = partition(arr, i, j);
+            if (p < k) i = p + 1;
+            else if (p > k) j = p - 1;
+            else return arr[p];
+        }
+    }
+
+    private static class Range {
+        public int start, end;
+
+        public Range(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+    private static <Key extends Comparable<Key>> Range threeWayPartition(Key[] arr, int lo, int hi) {
+        int k = lo, i = lo + 1, j = hi;
+        while (i <= j) {
+            if (less(arr, i, k)) swap(arr, i++, k++);
+            else if (greater(arr, i, k)) swap(arr, i, j--);
+            else i++;
+        }
+        return new Range(k, i);
+    }
+
+    private static <Key extends Comparable<Key>> void threeWaySort(Key[] arr, int lo, int hi) {
+        if (lo >= hi) return;
+        Range range = threeWayPartition(arr, lo, hi);
+        threeWaySort(arr, lo, range.start - 1);
+        threeWaySort(arr, range.end + 1, hi);
+    }
+
+    public static <Key extends Comparable<Key>> void threeWaySort(Key[] arr) {
+        shuffle(arr);
+        threeWaySort(arr, 0, arr.length - 1);
     }
 }
