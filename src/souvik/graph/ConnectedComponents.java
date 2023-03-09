af8a373e71098ec7e5d5609f4c50083dbd1f7c63
Souvik Sarkar <souviksarkar2k3@gmail.com>
2023-03-09 11:11:27 +0530

    Strong Components Implementation and Connected Components Readability Improved.

--- src/souvik/graph/ConnectedComponents.java
+++ src/souvik/graph/ConnectedComponents.java
@@ -2,12 +2,12 @@ package souvik.graph;
 
 public class ConnectedComponents {
     private final boolean[] marked;
-    private final int[] id;
+    private final int[] cc;
     private int count;
 
     public ConnectedComponents(GraphAL graphAL) {
         marked = new boolean[graphAL.vertices()];
-        id = new int[graphAL.vertices()];
+        cc = new int[graphAL.vertices()];
         for (int v = 0; v < graphAL.vertices(); v++) {
             if (!marked[v]) {
                 visit(graphAL, v);
@@ -18,7 +18,7 @@ public class ConnectedComponents {
 
     public ConnectedComponents(WeightedGraphAL weightedGraphAL) {
         marked = new boolean[weightedGraphAL.vertices()];
-        id = new int[weightedGraphAL.vertices()];
+        cc = new int[weightedGraphAL.vertices()];
         for (int v = 0; v < weightedGraphAL.vertices(); v++) {
             if (!marked[v]) {
                 visit(weightedGraphAL, v);
@@ -29,7 +29,7 @@ public class ConnectedComponents {
 
     private void visit(GraphAL graphAL, int v) {
         marked[v] = true;
-        id[v] = count;
+        cc[v] = count;
         for (int w : graphAL.adj(v)) {
             if (!marked[w]) visit(graphAL, w);
         }
@@ -37,7 +37,7 @@ public class ConnectedComponents {
 
     private void visit(WeightedGraphAL weightedGraphAL, int v) {
         marked[v] = true;
-        id[v] = count;
+        cc[v] = count;
         for (Edge e : weightedGraphAL.adj(v)) {
             int w = e.other(v);
             if (!marked[w]) visit(weightedGraphAL, w);
@@ -45,10 +45,14 @@ public class ConnectedComponents {
     }
 
     public int id(int v) {
-        return id[v];
+        return cc[v];
     }
 
     public boolean isConnected(int v, int w) {
-        return id[v] == id[w];
+        return cc[v] == cc[w];
+    }
+
+    public int count() {
+        return count;
     }
 }
--- src/souvik/graph/StrongComponents.java
+++ src/souvik/graph/StrongComponents.java
@@ -1,5 +1,76 @@
 package souvik.graph;
 
+import souvik.support.Stack;
+
 public class StrongComponents {
-    
+    private final boolean[] marked;
+    private final int[] scc;
+    private int count;
+
+    public StrongComponents(Digraph digraph) {
+        marked = new boolean[digraph.vertices()];
+        scc = new int[digraph.vertices()];
+        Topology tp = new Topology(digraph.reverse());
+        for (int v : tp.order()) {
+            if (!marked[v]) {
+                dfs(digraph, v);
+                count++;
+            }
+        }
+    }
+
+    private void dfs(Digraph digraph, int source) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(source);
+        while (!stack.isEmpty()) {
+            int v = stack.pop();
+            marked[v] = true;
+            scc[v] = count;
+            for (int w : digraph.adj(v)) {
+                if (!marked[w]) {
+                    stack.push(w);
+                }
+            }
+        }
+    }
+
+    public StrongComponents(WeightedDigraph weightedDigraph) {
+        marked = new boolean[weightedDigraph.vertices()];
+        scc = new int[weightedDigraph.vertices()];
+        Topology tp = new Topology(weightedDigraph.reverse());
+        for (int v : tp.order()) {
+            if (!marked[v]) {
+                dfs(weightedDigraph, v);
+                count++;
+            }
+        }
+    }
+
+    private void dfs(WeightedDigraph weightedDigraph, int source) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(source);
+        while (!stack.isEmpty()) {
+            int v = stack.pop();
+            marked[v] = true;
+            scc[v] = count;
+            for (DirectedEdge e : weightedDigraph.adj(v)) {
+                if (!marked[e.to()]) {
+                    stack.push(e.to());
+                }
+            }
+        }
+    }
+
+    public int id(int v) {
+        return scc[v];
+    }
+
+    public boolean isConnected(int v, int w) {
+        return scc[v] == scc[w];
+    }
+
+    public int count() {
+        return count;
+    }
+
 }
