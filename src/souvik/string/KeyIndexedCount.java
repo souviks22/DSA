e3aebc41c95d7bcbc82fcd7fb5d938125aa8d212
Souvik Sarkar <souviksarkar2k3@gmail.com>
2023-04-23 01:47:17 +0530

    Key Indexed Counting Sort Implementation for Characters.

---
+++ src/souvik/string/KeyIndexedCount.java
@@ -0,0 +1,17 @@
+package souvik.string;
+
+import java.util.Arrays;
+
+public class KeyIndexedCount {
+    private KeyIndexedCount() {
+    }
+
+    public void sort(char[] arr) {
+        final int R = 256;
+        int[] count = new int[R + 1];
+        char[] aux = Arrays.copyOf(arr, arr.length);
+        for (char c : aux) count[c + 1]++;
+        for (int i = 0; i < R; i++) count[i + 1] += count[i];
+        for (char c : aux) arr[count[c]++] = c;
+    }
+}
