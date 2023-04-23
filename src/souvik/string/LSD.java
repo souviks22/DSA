7f5d2113a3c512c2e45804135757295dbd63ecde
Souvik Sarkar <souviksarkar2k3@gmail.com>
2023-04-23 14:37:37 +0530

    Least Significant Digit First Sorting Implementation.

--- src/souvik/string/LSD.java
+++ src/souvik/string/LSD.java
@@ -1,2 +1,23 @@
-package souvik.string;public class LSD {
+package souvik.string;
+
+import java.util.Arrays;
+
+public class LSD {
+    private LSD() {
+    }
+
+    public static void sort(MyString[] arr) {
+        int maxLength = Integer.MIN_VALUE;
+        for (MyString str : arr) {
+            if (str.length() > maxLength) maxLength = str.length();
+        }
+        final int R = 256;
+        for (int d = maxLength - 1; d >= 0; d--) {
+            int[] count = new int[R + 1];
+            MyString[] aux = Arrays.copyOf(arr, arr.length);
+            for (MyString str : aux) count[str.charAt(d) + 1]++;
+            for (int i = 0; i < R; i++) count[i + 1] += count[i];
+            for (MyString str : aux) arr[count[str.charAt(d)]++] = str;
+        }
+    }
 }
