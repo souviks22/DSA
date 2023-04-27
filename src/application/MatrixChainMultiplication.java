36ad7c2664b5bbdd110cea9ec8a4b74d25357ec3
Souvik Sarkar <souviksarkar2k3@gmail.com>
2023-04-27 21:27:26 +0530

    Matrix Chain Multiplication Refactored.

--- src/application/MatrixChainMultiplication.java
+++ src/application/MatrixChainMultiplication.java
@@ -2,29 +2,31 @@ package application;
 
 public class MatrixChainMultiplication {
     private final int minMultiplications;
+    private final int[] orders;
+    private final int[][] dp, brackets;
     private final StringBuilder multiplicationOrder;
-    private final Integer[][] dp, brackets;
     private char matrix = 'A';
 
     public MatrixChainMultiplication(int[] orders) {
-        dp = new Integer[orders.length][orders.length];
-        brackets = new Integer[orders.length][orders.length];
+        this.orders = orders;
+        this.dp = new int[orders.length][orders.length];
+        this.brackets = new int[orders.length][orders.length];
         for (int i = 0; i < orders.length; i++) {
-            dp[i] = new Integer[orders.length];
-            brackets[i] = new Integer[orders.length];
+            dp[i] = new int[orders.length];
+            brackets[i] = new int[orders.length];
         }
-        this.minMultiplications = minMultiplications(orders, 1, orders.length - 1);
+        this.minMultiplications = minMultiplications(1, orders.length - 1);
         this.multiplicationOrder = new StringBuilder();
         setMultiplicationsOrder(1, orders.length - 1);
     }
 
-    private int minMultiplications(int[] orders, int start, int end) {
+    private int minMultiplications(int start, int end) {
         if (start == end) return 0;
-        if (dp[start][end] != null) return dp[start][end];
+        if (dp[start][end] != 0) return dp[start][end];
         double currentMin = Double.POSITIVE_INFINITY;
         for (int i = start; i < end; i++) {
-            dp[start][i] = minMultiplications(orders, start, i);
-            dp[i + 1][end] = minMultiplications(orders, i + 1, end);
+            dp[start][i] = minMultiplications(start, i);
+            dp[i + 1][end] = minMultiplications(i + 1, end);
             double provisionalMin = dp[start][i] + dp[i + 1][end] + orders[start - 1] * orders[i] * orders[end];
             if (provisionalMin < currentMin) {
                 currentMin = provisionalMin;
