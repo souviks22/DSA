5f30aa82bcd54f9664094f373229aba12c45958b
Souvik Sarkar <souviksarkar2k3@gmail.com>
2025-03-14 15:22:21 +0530

    Driver code for Red Black BST

--- src/souvik/bst/RedBlackBST.java
+++ src/souvik/bst/RedBlackBST.java
@@ -2,29 +2,18 @@ package souvik.bst;
 
 public class RedBlackBST<Key extends Comparable<Key>, Value> {
     private Node root;
-    private final boolean RED = true;
+    private final boolean RED = true, BLACK = false;
 
     private class Node {
         public Key key;
         public Value value;
-        public boolean color;
+        public boolean color = RED;
         public Node left, right;
 
         public Node(Key key, Value value) {
             this.key = key;
             this.value = value;
-            color = RED;
-            left = null;
-            right = null;
         }
-    }
-
-    public RedBlackBST(Node root) {
-        this.root = root;
-    }
-
-    public RedBlackBST() {
-        this(null);
     }
 
     public Value find(Key key) {
@@ -39,33 +28,30 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
     }
 
     private Node rotateLeft(Node node) {
-        assert node.right.color == RED;
-        Node rightNode = node.right;
-        node.right = rightNode.left;
-        rightNode.left = node;
-        rightNode.color = node.color;
+        Node right = node.right;
+        node.right = right.left;
+        right.left = node;
+        right.color = node.color;
         node.color = RED;
-        return rightNode;
+        return right;
     }
 
     private Node rotateRight(Node node) {
-        assert node.left.color == RED;
-        Node leftNode = node.left;
-        node.left = leftNode.right;
-        leftNode.right = node;
-        leftNode.color = node.color;
+        Node left = node.left;
+        node.left = left.right;
+        left.right = node;
+        left.color = node.color;
         node.color = RED;
-        return leftNode;
+        return left;
     }
 
     private void flipColor(Node node) {
-        assert node.color == !RED && node.left.color == RED && node.right.color == RED;
         node.color = RED;
-        node.left.color = node.right.color = !RED;
+        node.left.color = node.right.color = BLACK;
     }
 
     private boolean isRed(Node node) {
-        return node.color == RED;
+        return node != null && node.color == RED;
     }
 
     public void insert(Key key, Value value) {
@@ -82,5 +68,19 @@ public class RedBlackBST<Key extends Comparable<Key>, Value> {
         if (isRed(node.left) && isRed(node.left.left)) node = rotateRight(node);
         if (isRed(node.left) && isRed(node.right)) flipColor(node);
         return node;
+    }
+
+    public static void main(String[] args) {
+        int n = 10000;
+        BinarySearchTree<Integer,String> bst = new BinarySearchTree<>();
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < n; ++i) bst.insert(i,"string");
+        long end = System.currentTimeMillis();
+        System.out.println("BST : " + (end - start) + " ms");
+        RedBlackBST<Integer,String> rbt = new RedBlackBST<>();
+        start = System.currentTimeMillis();
+        for (int i = 0; i < n; ++i) rbt.insert(i,"string");
+        end = System.currentTimeMillis();
+        System.out.println("RBT : " + (end - start) + " ms");
     }
 }
