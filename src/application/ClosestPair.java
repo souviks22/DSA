57caf11f2afc4c899f70f2edb8308496b850a59c
Souvik Sarkar <souviksarkar2k3@gmail.com>
2025-03-16 13:51:35 +0530

    Closest Pair Modification

--- src/application/ClosestPair.java
+++ src/application/ClosestPair.java
@@ -2,80 +2,74 @@ package application;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Random;
 
 public class ClosestPair {
-    private final int[][] pointsX, pointsY, aux;
-    private final double closestDistance;
+    private final int[][] points, aux;
+    private final double closestDist;
 
     public ClosestPair(int[][] points) {
-        int n = points.length;
-        pointsX = new int[n][2];
-        pointsY = new int[n][2];
-        aux = new int[n][2];
-        for (int i = 0; i < n; ++i) {
-            pointsX[i] = points[i];
-            pointsY[i] = points[i];
-        }
-        Arrays.sort(pointsX, new XComparator());
-        Arrays.sort(pointsY, new YComparator());
-        closestDistance = minDist(0, n - 1);
+        Arrays.sort(points, Comparator.comparingInt(point -> point[0]));
+        this.points = points;
+        aux = new int[points.length][2];
+        closestDist = minimize(0, points.length - 1);
     }
 
-    private double minDist(int lo, int hi) {
-        if (hi - lo + 1 <= 3) return naive(lo, hi, pointsY);
-        for (int k = lo; k <= hi; ++k) aux[k] = pointsY[k];
-        int mid = lo + (hi - lo) / 2;
-        int i = lo, j = mid + 1, midX = pointsX[mid][0];
-        for (int k = lo; k <= hi; ++k) {
-            if (i > mid) pointsY[j++] = aux[k];
-            else if (j > hi) pointsY[i++] = aux[k];
-            else if (aux[k][0] <= midX) pointsY[i++] = aux[k];
-            else pointsY[j++] = aux[k];
+    private double minimize(int lo, int hi) {
+        if (lo == hi) return Double.POSITIVE_INFINITY;
+        if (hi - lo + 1 == 2) {
+            if (points[lo][1] > points[hi][1]) {
+                int[] temp = points[lo];
+                points[lo] = points[hi];
+                points[hi] = temp;
+            }
+            return dist(points[lo], points[hi]);
         }
-        double mini = Math.min(minDist(lo, mid), minDist(mid + 1, hi));
-        int slo = -1, shi = -1;
+        int mid = lo + (hi - lo)/2;
+        int midX = points[mid][0];
+        double d = Math.min(minimize(lo, mid), minimize(mid + 1, hi));
+        System.arraycopy(points, lo, aux, lo, hi - lo + 1);
+        int i = lo, j = mid + 1;
         for (int k = lo; k <= hi; ++k) {
-            if (Math.abs(pointsX[k][0] - midX) <= mini) {
-                if (slo == -1) slo = k;
-                shi = k;
+            if (i > mid) points[j++] = aux[k];
+            else if (j > hi) points[i++] = aux[k];
+            else if (aux[i][1] < aux[j][1]) points[i++] = aux[k];
+            else points[j++] = aux[k];
+        }
+        int n = 0;
+        for (int k = lo; k <= hi; ++k) {
+            if (Math.abs(points[k][0] - midX) < d) {
+                aux[n++] = points[k];
             }
         }
-        return Math.min(naive(slo, shi, pointsX), mini);
-    }
-
-    private double naive(int lo, int hi, int[][] points) {
-        double mini = Double.POSITIVE_INFINITY;
-        for (int i = lo; i <= hi; ++i) {
-            for (int j = i + 1; j <= hi; ++j) {
-                mini = Math.min(dist(points[i], points[j]), mini);
+        double mini = d;
+        for (int k = 0; k < n; ++k) {
+            double limit = aux[k][1] - d;
+            for (int l = k-1; l >= 0 && aux[l][1] > limit; --l) {
+                mini = Math.min(dist(aux[k], aux[l]), mini);
             }
         }
         return mini;
     }
 
     private double dist(int[] point1, int[] point2) {
-        int dx = point1[0] - point2[0], dy = point1[1] - point2[1];
+        long dx = point1[0] - point2[0], dy = point1[1] - point2[1];
         return Math.sqrt(dx * dx + dy * dy);
     }
 
-    private static class XComparator implements Comparator<int[]> {
-        public int compare(int[] point1, int[] point2) {
-            return Integer.compare(point1[0], point2[0]);
-        }
-    }
-
-    private static class YComparator implements Comparator<int[]> {
-        public int compare(int[] point1, int[] point2) {
-            return Integer.compare(point1[1], point2[1]);
-        }
-    }
-
     public double getClosestDistance() {
-        return closestDistance;
+        return closestDist;
     }
 
     public static void main(String[] args) {
-        int[][] points = {{2, 3}, {12, 30}, {40, 50}, {5, 1}, {12, 10}, {3, 4}};
+        int n = 1000000;
+        int[][] points = new int[n][];
+        Random random = new Random();
+        for (int i = 0; i < n; ++i) {
+            int x = random.nextInt(-n,n);
+            int y = random.nextInt(-n,n);
+            points[i] = new int[]{x,y};
+        }
         ClosestPair closestPair = new ClosestPair(points);
         System.out.println(closestPair.getClosestDistance());
     }
